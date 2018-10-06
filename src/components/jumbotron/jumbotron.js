083fe83513e877ccadfd89f02d5853b08808b4d8
unknown <monmaramba@yahoo.com>
2018-10-05 20:39:50 -0700

    components, index and jumbotron

---
+++ src/components/jumbotron/jumbotron.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./jumbotron.css";
+
+const jumbotron = () => (
+    <div className = "jumbotron jumbotron-fluid">
+        <div className="container text-center">
+            <h1 className="display-4">The Clicky Game!</h1>
+            <p className="lead">Earn points by clicking on the images</p>
+        </div>
+    </div>
+);
+
+export default jumbotron;
