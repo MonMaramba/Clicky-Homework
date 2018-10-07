7dc381e231b5d27d17b0e81fa9a6ccc613de1929
unknown <monmaramba@yahoo.com>
2018-10-06 17:12:51 -0700

    for heroku deployment

---
+++ build/precache-manifest.7e5c8d4dd0342769706bf4b0ed81f8f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3069974c1686e6e17f7c",
+    "url": "/static/js/main.3069974c.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "3069974c1686e6e17f7c",
+    "url": "/static/css/main.b225ccbb.chunk.css"
+  },
+  {
+    "revision": "bd1e5e78b4503e147efea0cf52a89ac8",
+    "url": "/index.html"
+  }
+];
