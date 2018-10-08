7008e852432fe1b5032eaa78902e4b4a60b04935
unknown <monmaramba@yahoo.com>
2018-10-07 17:15:37 -0700

    Updates

---
+++ precache-manifest.b56578017b4ba71783216e4b7d3fa4db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "51df9c73e98f4bd95e25",
+    "url": "/clicky-homework/static/js/runtime~main.51df9c73.js"
+  },
+  {
+    "revision": "80693c0f94a6a49cb455",
+    "url": "/clicky-homework/static/js/main.80693c0f.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/clicky-homework/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "80693c0f94a6a49cb455",
+    "url": "/clicky-homework/static/css/main.5313c770.chunk.css"
+  },
+  {
+    "revision": "f204d046e92fffb0c05acb7d8bc1e39d",
+    "url": "/clicky-homework/index.html"
+  }
+];
